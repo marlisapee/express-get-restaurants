9b98846096717e993d1f986477331fa759aa4c3e
marlisapee <mpee@tripadvisor.com>
2024-05-06 18:24:42 -0400

    added get req for all restaurants

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const app = require("./src/app")
-const db = require("./db/connection");
+const app = require('./src/app');
+const db = require('./db/connection');
+const Restaurant = require('./models');
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
 
+app.get('/restaurants', async (req, res) => {
+  const listOfRestaurants = await Restaurant.findAll();
+  res.json(listOfRestaurants);
+});
 
 app.listen(port, () => {
-    db.sync();
-    console.log(`Listening at http://localhost:${port}/restaurants`);
-})
+  db.sync();
+  console.log(`Listening at http://localhost:${port}/restaurants`);
+});
